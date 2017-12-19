b56616344353ec3070a69f394d8ed1216f720407
ojlee <dldhwn56@gmail.com>
2017-12-19 10:58:30 +0900

    전체화면 오류수정

--- gulpfile.js
+++ gulpfile.js
@@ -45,7 +45,7 @@ gulp.task('watch', function () {
 //gulp를 실행하면 default 로 uglify task와 watch task를 실행
 gulp.task('default', ['uglify', 'watch', 'compress-html', 'minify-css', 'copy-lib']);
 
-gulp.task('deploy', ['default'], () => {
-return gulp.src('dist/**/*')
-  .pipe($.ghPages());
+gulp.task('deploy', function() {
+  return gulp.src('./dist/**/*')
+    .pipe(ghPages());
 });
